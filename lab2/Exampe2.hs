248857cec79c09d97f1f295dd5425eca924ef791
sam <samueljlevin@gmail.com>
2016-02-16 14:05:30 -0500

    Changes of files

---
+++ lab2/Exampe2.hs
@@ -0,0 +1,26 @@
+
+module Example2 where
+
+scopeTest :: Integer
+scopeTest = 
+	let x = 3 in
+		let f1 = x
+		    f2 = 
+		    	let x = 4 in
+		    		f1			in
+		f2
+		    
+			
+f x = "I am not using argument x"
+
+intsFrom :: Integer -> [Integer]
+intsFrom n = n : (intsFrom (n+1))
+
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+sieve (n:ns) = n: sieve (filter (notDiv n) ns) where
+	notDiv n x = x `mod` n /= 0
+	
+primes = sieve [2..]
+
+data Tree a = Leaf a | Node a (Tree a) (Tree a) deriving Show
